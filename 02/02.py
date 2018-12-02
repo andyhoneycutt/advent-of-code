e7a3b2cc0d6637b8071d8fbf7c98d389e641123f
Andy Honeycutt <ahoneycutt@accendero.com>
2018-12-01 23:08:23 -0700

    remove unused vars

--- 02/02.py
+++ 02/02.py
@@ -4,8 +4,6 @@ import functions
 
 if __name__ == '__main__':
     filename = os.path.join(functions.get_path(__file__), 'input.txt')
-    twos = 0
-    threes = 0
     data = functions.read_file(filename)
     answer = functions.get_similar_string(data)
     print(answer)
