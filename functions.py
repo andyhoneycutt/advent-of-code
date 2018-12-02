64bd3189574e09c59c80332e88d3196356c098cf
Andy Honeycutt <ahoneycutt@accendero.com>
2018-12-02 14:11:03 -0700

    remove unnecessary function

--- functions.py
+++ functions.py
@@ -5,16 +5,8 @@ def get_path(file):
     return os.path.dirname(os.path.realpath(file))
 
 
-def str_to_num(s):
-    op = s[0]
-    val = int(s[1:])
-    if op == '+':
-        return val
-    return val * -1
-
-
 def ftoi(file):
-    return [str_to_num(l.strip()) for l in read_file(file)]
+    return [int(l.strip()) for l in read_file(file)]
 
 
 def read_file(file):
