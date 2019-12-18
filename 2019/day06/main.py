351b7374ac7f13e98fda97ac966ae7c5362b552d
Andy Honeycutt <ahoneycutt@accendero.com>
2019-12-18 16:15:01 -0700

    add part two with testing

--- 2019/day06/main.py
+++ 2019/day06/main.py
@@ -15,9 +15,46 @@ def get_len(origin, length, orbs):
         return length
 
 
+def get_orbit_for(satellite: str, orbs: dict) -> str:
+    for k, v in orbs.items():
+        if satellite in v:
+            return k
+
+
+def get_path_for(satellite: str, orbs: dict) -> list:
+    orbit = get_orbit_for(satellite, orbs)
+    path = [orbit]
+    while orbit:
+        orbit = get_orbit_for(orbit, orbs)
+        if orbit:
+            path.append(orbit)
+    return path
+
+
+def len_until(lst, v):
+    i = 0
+    for c in lst:
+        if c == v:
+            return i
+        i += 1
+
+
+def get_transfers(origin, target, orbs):
+    path_origin = get_path_for(origin, orbs)
+    path_target = get_path_for(target, orbs)
+    match = [v for v in path_origin if v in path_target]
+    node = match[0]
+    return len_until(path_origin, node) + len_until(path_target, node)
+
+
 def part_one(orbs):
     orbs = get_orbits(orbs)
     print(get_len('COM', 0, orbs))
+
+
+def part_two(orbs):
+    orbs = get_orbits(orbs)
+    print(get_transfers('YOU', 'SAN', orbs))
 
 
 if __name__ == '__main__':
@@ -25,3 +62,4 @@ if __name__ == '__main__':
     with open('input.txt', 'r') as fp:
         orbits = fp.read().split('\n')
     part_one(list(orbits))
+    part_two(list(orbits))
--- 2019/day06/tests.py
+++ 2019/day06/tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from day06.main import get_orbits, get_len
+from day06.main import get_orbits, get_len, get_transfers
 
 
 class PartOneTests(TestCase):
@@ -20,3 +20,24 @@ class PartOneTests(TestCase):
         ]
         orbits = get_orbits(sample)
         self.assertEqual(42, get_len('COM', 0, orbits))
+
+
+class PartTwoTest(TestCase):
+    def test_sample(self):
+        sample = [
+            'COM)B',
+            'B)C',
+            'C)D',
+            'D)E',
+            'E)F',
+            'B)G',
+            'G)H',
+            'D)I',
+            'E)J',
+            'J)K',
+            'K)L',
+            'K)YOU',
+            'I)SAN',
+        ]
+        orbits = get_orbits(sample)
+        self.assertEqual(4, get_transfers('YOU', 'SAN', orbits))
