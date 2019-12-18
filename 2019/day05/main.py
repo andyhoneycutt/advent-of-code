2a5caa4e82a055248588878d8c047107a038a5ee
Andy Honeycutt <ahoneycutt@accendero.com>
2019-12-18 14:54:12 -0700

    run part 2

--- 2019/day05/main.py
+++ 2019/day05/main.py
@@ -79,6 +79,11 @@ def part_one(inst):
     run(inst, 1)
 
 
+def part_two(inst):
+    run(inst, 5)
+
+
 if __name__ == '__main__':
     instructions = input_to_codes(read_input('input.txt'))
-    part_one(instructions)
+    part_one(list(instructions))
+    part_two(list(instructions))
