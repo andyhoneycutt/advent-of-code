b83fdf9ecd5a8872583de760cf299aad4a5e5975
Andy Honeycutt <ahoneycutt@accendero.com>
2019-12-18 19:57:04 -0700

    begin part 2, require signal range as input

--- 2019/day07/main.py
+++ 2019/day07/main.py
@@ -15,12 +15,13 @@ def list_to_int(lst: list) -> int:
     return int(''.join(lst))
 
 
-def get_thruster_signal(inst, phase):
+def get_thruster_signal(inst, phase, copy_sequence=True):
     p = iter(phase)
     c = next(p), 0
     thrust = 0
     for i in range(5):
-        thrust = run(list(inst), iter(c))
+        seq = copy_sequence and list(inst) or inst
+        thrust = run(seq, iter(c))
         try:
             c = next(p), thrust
         except StopIteration:
@@ -37,12 +38,21 @@ def get_max_thruster_signal(sequence: list, signal_range: range) -> int:
     return max_signal
 
 
+def run_signal_to_halt(sequence: list, signal_range: range) -> int:
+    signals = get_signals(signal_range)
+    max_signal = 0
+    for phase in signals:
+        signal = get_thruster_signal(sequence, phase)
+        max_signal = signal > max_signal and signal or max_signal
+    return max_signal
+
+
 def part_one(inst):
     print(get_max_thruster_signal(list(inst), signal_range=range(5)))
 
 
-def part_two():
-    pass
+def part_two(inst):
+    print(run_signal_to_halt(list(inst), signal_range=range(5, 10)))
 
 
 if __name__ == '__main__':
--- 2019/day07/tests.py
+++ 2019/day07/tests.py
@@ -24,10 +24,31 @@ class PartOneTests(TestCase):
         self.assertEqual(65210, get_thruster_signal(self.c, [1, 0, 4, 3, 2]))
 
     def test_get_max_thruster_signal(self):
-        self.assertEqual(43210, get_max_thruster_signal(self.a))
-        self.assertEqual(54321, get_max_thruster_signal(self.b))
-        self.assertEqual(65210, get_max_thruster_signal(self.c))
+        r = range(5)
+        self.assertEqual(43210, get_max_thruster_signal(self.a, r))
+        self.assertEqual(54321, get_max_thruster_signal(self.b, r))
+        self.assertEqual(65210, get_max_thruster_signal(self.c, r))
 
 
 class PartTwoTest(TestCase):
+    def setUp(self) -> None:
+        self.a = [
+            3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
+            27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5
+        ]
+        self.b = [
+            3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55,
+            1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008,
+            54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56,
+            1005, 56, 6, 99, 0, 0, 0, 0, 10
+        ]
+
+    def test_get_thruster_signal(self):
+        self.assertEqual(
+            139629729, get_thruster_signal(self.a, [9, 8, 7, 6, 5])
+        )
+        self.assertEqual(
+            18216, get_thruster_signal(self.b, [9, 7, 8, 5, 6])
+        )
+
     pass
