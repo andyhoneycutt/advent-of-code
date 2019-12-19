25af18425df9082466d81f2b89fe3abdb47e327e
Andy Honeycutt <ahoneycutt@accendero.com>
2019-12-19 07:18:29 -0700

    set up day 8 part 1

---
+++ 2019/day08/tests.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class PartOneTests(TestCase):
+    def setUp(self) -> None:
+        pass
+
+
+class PartTwoTest(TestCase):
+    def setUp(self) -> None:
+        pass
