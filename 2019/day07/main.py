c2c7c08bda98cde8199e701664c43aabeada9da2
Andy Honeycutt <ahoneycutt@accendero.com>
2019-12-18 18:57:09 -0700

    add day 7

---
+++ 2019/day07/main.py
@@ -0,0 +1,11 @@
+def part_one():
+    pass
+
+
+def part_two():
+    pass
+
+
+if __name__ == '__main__':
+    part_one()
+    part_two()
---
+++ 2019/day07/tests.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class PartOneTests(TestCase):
+    pass
+
+
+class PartTwoTest(TestCase):
+    pass
