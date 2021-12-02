b36f207f114c2416e5b3fceb1c82adb9c3ecacb6
Andy Honeycutt <ahoneycutt@accendero.com>
2021-12-01 21:41:19 -0700

    add day 01

---
+++ python/01/day_01.py
@@ -0,0 +1,25 @@
+def part_one(pings):
+    total = 0
+    for i, value in enumerate(pings):
+        try:
+            if pings[i+1] > value:
+                total += 1
+        except IndexError:
+            pass
+    return total
+
+
+def part_two(pings):
+    c = len(pings) - 3
+    sets = [pings[i:i+3] for i, _ in enumerate(pings) if i <= c]
+    sums = [sum(p) for p in sets]
+    return part_one(sums)
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as fp:
+        values = [int(ln) for ln in fp.readlines()]
+        one = part_one(values)
+        two = part_two(values)
+        print(one)
+        print(two)
