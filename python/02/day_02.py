40a8b2b5b1d4c86a841189fb1f971ff18cfd309d
Andy Honeycutt <ahoneycutt@accendero.com>
2021-12-01 22:10:00 -0700

    add day 02 part one

--- python/02/day_02.py
+++ python/02/day_02.py
@@ -1,8 +1,37 @@
 import time
 
 
-def part_one():
-    pass
+class Submarine:
+    commands: list = []
+    depth: int = 0
+    horizontal: int = 0
+
+    def __init__(self, commands):
+        self.commands = commands
+        self._cmd = {
+            'forward': self.forward,
+            'down': self.down,
+            'up': self.up,
+        }
+
+    def run(self):
+        for cmd, val in self.commands:
+            self._cmd[cmd](val)
+
+    def up(self, val):
+        self.depth -= val
+
+    def down(self, val):
+        self.depth += val
+
+    def forward(self, val):
+        self.horizontal += val
+
+
+def part_one(inputs):
+    submarine = Submarine(inputs)
+    submarine.run()
+    return submarine.horizontal * submarine.depth
 
 
 def part_two():
@@ -12,7 +41,12 @@ def part_two():
 if __name__ == '__main__':
     start = time.time()
     with open('input.txt', 'r') as fp:
-        part_one()
+        values = []
+        for line in fp.readlines():
+            cmd, val = line.split(' ')
+            values.append((cmd, int(val)))
+        one = part_one(values)
+        print(one)
         part_two()
     end = time.time()
     tot = end - start
