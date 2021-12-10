496a1dfb318e5cc08cfc5d8a2634c5947ef5b20c
Andy Honeycutt <ahoneycutt@accendero.com>
2021-12-10 07:38:54 -0700

    add day 10

--- python/2021/10/main.py
+++ python/2021/10/main.py
@@ -1,11 +1,61 @@
 import time
 
+SCORES = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+}
+
+AUTO_SCORES = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+}
+
+VALID = ('[]', '{}', '<>', '()')
+
+def get_score(ln):
+    for c in ln:
+        try:
+            return SCORES[c]
+        except KeyError:
+            pass
+    return 0
+
+def get_autocomplete_score(ln):
+    score = 0
+    for c in ln:
+        score *= 5
+        score += AUTO_SCORES[c]
+    return score
+
+
+def parse_input(ln):
+    pairs = {a: b for a, b in VALID}
+    expected = []
+    for c in ln:
+        if c in pairs:
+            expected.append(c)
+            continue
+        if c != pairs[expected.pop()]:
+            return c, None
+    return None, reversed([pairs[c] for c in expected])
+
+
 def part_one(inputs):
-    return inputs
+    parsed = [p for p, _ in [parse_input(ln) for ln in inputs]]
+    score = sum([get_score(ln) for ln in parsed if ln])
+    return score
 
 
 def part_two(inputs):
-    return inputs
+    lines = [b for a, b in [parse_input(ln) for ln in inputs] if not a]
+    scores = sorted([get_autocomplete_score(ln) for ln in lines])
+    i = len(scores) // 2
+    return scores[i]
+
 
 def main():
     with open('input.txt', 'r') as fp:
--- python/2021/10/test.py
+++ python/2021/10/test.py
@@ -1,15 +1,27 @@
 import unittest
 from main import part_one, part_two
 
-INPUT = True
+INPUT = [
+    '[({(<(())[]>[[{[]{<()<>>',
+    '[(()[<>])]({[<{<<[]>>(',
+    '{([(<{}[<>[]}>{[]{[(<()>',
+    '(((({<>}<{<{<>}{[]{[]{}',
+    '[[<[([]))<([[{}[[()]]]',
+    '[{[{({}]{}}([{[{{{}}([]',
+    '{<[[]]>}<{[{[{[]{()[[[]',
+    '[<(<(<(<{}))><([]([]()',
+    '<{([([[(<>()){}]>(<<{{',
+    '<{([{{}}[<[[[<>{}]]]>[]]',
+]
+
 
 class TestDay09(unittest.TestCase):
     def test_part_one(self):
-        expected = True
+        expected = 26397
         actual = part_one(INPUT)
         self.assertEqual(expected, actual)
 
     def test_part_two(self):
-        expected = True
+        expected = 288957
         actual = part_two(INPUT)
         self.assertEqual(expected, actual)
