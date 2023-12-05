aa295da6b6220e51100a3bbc997fe362921d752c
Andy Honeycutt <ahoneycutt@accendero.com>
2023-12-05 08:41:48 -0700

    2023: add day 5 -- almost working, part 2

---
+++ python/2023/05/main.py
@@ -0,0 +1,114 @@
+import time
+
+DEBUG = False
+
+relationships = {
+    "seeds": "seed-to-soil",
+    "seed-to-soil": "soil-to-fertilizer",
+    "soil-to-fertilizer": "fertilizer-to-water",
+    "fertilizer-to-water": "water-to-light",
+    "water-to-light": "light-to-temperature",
+    "light-to-temperature": "temperature-to-humidity",
+    "temperature-to-humidity": "humidity-to-location",
+}
+
+
+def read_almanac(lines: list[str]):
+    ln = lines.pop(0)
+    name = ln.split(":")[0].replace(" map", "")
+    ranges = []
+    while True:
+        if "map" not in ln:
+            ranges.append([int(x) for x in ln.split(":")[1].split()])
+        try:
+            line = lines.pop(0)
+        except IndexError:
+            break
+        if line == "":
+            break
+        ranges.append([int(x) for x in line.split()])
+    return Almanac(name, ranges)
+
+
+class Almanac:
+    name: str
+    ranges: list
+
+    def __init__(self, name: str, ranges: list):
+        self.name = name
+        self.ranges = ranges
+
+    def __str__(self):
+        return f"{self.name}: {self.ranges}"
+
+
+def part_one(inputs):
+    seeds = read_almanac(inputs)
+    almanacs = []
+    while inputs:
+        almanac = read_almanac(inputs)
+        almanacs.append(almanac)
+
+    locations = []
+    for seed in seeds.ranges[0]:
+        if DEBUG:
+            print(f"seed: {seed}")
+        for almanac in almanacs:
+            for dest, src, rng in almanac.ranges:
+                if src <= seed <= src + rng:
+                    diff = seed - src
+                    if DEBUG:
+                        print(f"{seed} -> {dest + diff}")
+                    seed = dest + diff
+                    break
+        locations.append(seed)
+    return min(locations)
+
+
+def part_two(inputs):
+    seeds = read_almanac(inputs)
+    almanacs = []
+    while inputs:
+        almanac = read_almanac(inputs)
+        almanacs.append(almanac)
+
+    locations = []
+    seed_ranges = iter(seeds.ranges[0])
+    while True:
+        try:
+            strt = next(seed_ranges)
+            length = next(seed_ranges)
+        except StopIteration:
+            break
+        for almanac in almanacs:
+            for dest, src, rng in almanac.ranges:
+                a1, a2 = strt, strt + length
+                b1, b2 = src, src + rng
+                # some of the seed range overlaps with almanac range
+                if a1 <= b2 and b1 <= a2:
+                    # get the least number in the ranges
+                    least = min(a2, b2) - max(a1, b1)
+                    strt = dest + least
+                    break
+
+        locations.append(strt)
+    if DEBUG:
+        print(locations)
+    return min([l for l in locations if l > 0])
+
+
+def main():
+    with open('input.txt', 'r', encoding="utf-8") as fp:
+        lines = [l.strip() for l in fp.readlines()]
+        one = part_one(lines.copy())
+        print(one)
+        two = part_two(lines.copy())
+        print(two)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    end = time.time()
+    tot = end - start
+    print(f'Completed in {tot:.6f} seconds')
---
+++ python/2023/05/test.py
@@ -0,0 +1,52 @@
+import unittest
+
+from main import part_one, part_two
+
+
+class TestDay05(unittest.TestCase):
+    def setUp(self):
+        self.input = [
+            "seeds: 79 14 55 13",
+            "",
+            "seed-to-soil map:",
+            "50 98 2",
+            "52 50 48",
+            "",
+            "soil-to-fertilizer map:",
+            "0 15 37",
+            "37 52 2",
+            "39 0 15",
+            "",
+            "fertilizer-to-water map:",
+            "49 53 8",
+            "0 11 42",
+            "42 0 7",
+            "57 7 4",
+            "",
+            "water-to-light map:",
+            "88 18 7",
+            "18 25 70",
+            "",
+            "light-to-temperature map:",
+            "45 77 23",
+            "81 45 19",
+            "68 64 13",
+            "",
+            "temperature-to-humidity map:",
+            "0 69 1",
+            "1 0 69",
+            "",
+            "humidity-to-location map:",
+            "60 56 37",
+            "56 93 4",
+        ]
+
+    def test_part_one(self):
+        expected = 35
+        actual = part_one(self.input)
+        self.assertEqual(expected, actual)
+
+    def test_part_two(self):
+        expected = 46
+        actual = part_two(self.input)
+        self.assertEqual(expected, actual)
